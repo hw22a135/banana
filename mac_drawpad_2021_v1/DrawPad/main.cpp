0afeea2a6873695bc0adfbc7141240cfeeaaf165
hw22a135 <147024126+hw22a135@users.noreply.github.com>
2023-10-12 14:10:52 +0900

    Update main.cpp

--- mac_drawpad_2021_v1/DrawPad/main.cpp
+++ mac_drawpad_2021_v1/DrawPad/main.cpp
@@ -23,7 +23,7 @@ void DrawMain()
         Paint(x+5, 0, kColorDarkBlue, kColorBlue);
     }
 
-    DrawText("Hello, world!!", -12*14/2, 44, kColorWhite);
+    DrawText("Hello, world!!", -12*14/2, 44, kColorWhite); //Draw Hello, world!! text
     Sleep(0.5f);
     DrawText("Hello, world!!", -12*14/2, 22, kColorYellow);
     Sleep(0.5f);
